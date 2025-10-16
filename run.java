f4614ad67e76b88c2a555d0d866446c0edba1d12
test-phonpon <phonpon.w@ku.th>
2025-10-16 18:03:47 +0700

    run.java

--- run.java
+++ run.java
@@ -1,11 +1,13 @@
-package car.test;
+package project;
 import javax.swing.*;
 
 public class run {
     public static void main(String[] args) {
         JFrame f = new JFrame("");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.add(new MapPanel());
+        f.setDefaultCloseOperation(3);
+        f.setResizable(false);
+        MapPanel map = new MapPanel();
+        f.add(map);
         f.pack();
         f.setLocationRelativeTo(null);
         f.setVisible(true);
